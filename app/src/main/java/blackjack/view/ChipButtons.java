e1f185866ff7cb100de5c030133c5a2dda238f17
Jack McPhillips <jackmcphillips@Jacks-MacBook-Pro.local>
2023-04-10 10:33:49 -0500

    adding the basic chips to the screen

--- app/src/main/java/blackjack/view/ChipButtons.java
+++ app/src/main/java/blackjack/view/ChipButtons.java
@@ -1,7 +1,6 @@
 package blackjack.view;
 
 import java.awt.*;
-import javax.swing.BoxLayout;
 import java.awt.event.*;
 import javax.swing.*;
 
@@ -9,17 +8,35 @@ public class ChipButtons extends JPanel{
     private JButton []chipValues;
 
     public ChipButtons(){
-        this.chipValues = new JButton[8];
+        this.chipValues = new JButton[5];
         String[] Values = {"$1","$5", "$10", "$20","$50", "$100", "$500", "$1,000"};
         int[] Colors = {0x4a071d, 0xeb1f10, 0xed9209, 0xf7e40a, 0x018508, 0x000000, 0x5e07ba, 0x4a071d};
         this.setLayout(new GridLayout(3, 3));
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        ImageIcon greenChip = new ImageIcon(classLoader.getResource("greenChip.png"));
+        ImageIcon orangeChip = new ImageIcon(classLoader.getResource("orangeChip.png"));
+        ImageIcon blueChip = new ImageIcon(classLoader.getResource("blueChip.png"));
+        ImageIcon redChip = new ImageIcon(classLoader.getResource("redChip.png"));
+        ImageIcon purpleChip = new ImageIcon(classLoader.getResource("purpleChip.png"));
+
+        ImageIcon[] chipPngs = {greenChip, orangeChip, blueChip, redChip, purpleChip};
         
-        for(int i = 0; i < 8; i++){
-            this.chipValues[i] = new JButton(Values[i]);
+        for(int i = 0; i < 5; i++){
+            resize(.1, chipPngs[i]);
+            this.chipValues[i] = new JButton(chipPngs[i]);
             this.chipValues[i].setPreferredSize(new Dimension(75,45));
-            this.chipValues[i].setForeground(new Color(Colors[i]));
             this.add(this.chipValues[i]);
         }
-        
+    }
+
+    public void resize(double scale, ImageIcon icon){
+      int height = icon.getIconHeight();
+      int width = icon.getIconWidth();
+      
+      height = (int)(height * scale);
+      width = (int)(width * scale);
+      Image image = icon.getImage().getScaledInstance(width, height, Image.SCALE_DEFAULT);
+      icon.setImage(image);
     }
 }
