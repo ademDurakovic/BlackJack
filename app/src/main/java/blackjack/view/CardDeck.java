09fed8dd4d41320f7e91ff28c883b55fef9e70d3
Adem Durakovic <adem.durakovic@slu.edu>
2023-04-14 12:41:09 -0500

    card Deck

--- app/src/main/java/blackjack/view/CardDeck.java
+++ app/src/main/java/blackjack/view/CardDeck.java
@@ -21,6 +21,7 @@ public class CardDeck {
         private final String[] seven_cards = {"7_of_spades.png", "7_of_hearts.png", "7_of_diamonds.png", "7_of_clubs.png"};
         private final String[] eight_cards = {"8_of_spades.png", "8_of_hearts.png", "8_of_diamonds.png", "8_of_clubs.png"};
         private final String[] nine_cards = {"9_of_spades.png", "9_of_hearts.png", "9_of_diamonds.png", "9_of_clubs.png"};
+
         private final String[] ten_cards = {"10_of_spades.png", "10_of_hearts.png", "10_of_diamonds.png", "10_of_clubs.png", 
             "king_of_spades.png", "king_of_hearts.png", "king_of_diamonds.png", "king_of_clubs.png",
             "queen_of_spades.png", "queen_of_hearts.png", "queen_of_diamonds.png", "queen_of_clubs.png",
@@ -30,18 +31,38 @@ public class CardDeck {
         private final String[] eleven_cards = {"ace_of_spades.png", "ace_of_hearts.png", "ace_of_diamonds.png", "ace_of_clubs.png"};
 
 
+        //currnet card PNG dealer and user.
+
     
 
-        private ArrayList<JLabel []> cardDeck = new ArrayList<JLabel []>();
+        private ArrayList<String []> cardDeck = new ArrayList<String []>();
+        Random rand;
 
         /*We make arrays of JLabels which we can add to in constructor. */
-     
-        
-
         public CardDeck(){
+
+            /*Adds each array to the arrayList. Each array holds Pngs of the images. */
+
+           
+            cardDeck.add(two_cards);  //index 0 
+            cardDeck.add(three_cards);
+            cardDeck.add(four_cards);
+            cardDeck.add(five_cards);
+            cardDeck.add(six_cards);
+            cardDeck.add(seven_cards);
+            cardDeck.add(eight_cards);
+            cardDeck.add(nine_cards);
+            cardDeck.add(ten_cards);
+            cardDeck.add(eleven_cards);  // index 9
+            rand = new Random();
             
-           /*  */
-      
+        }
+
+        public JLabel pullCard(int cardValue){
+            int realIndex = cardValue -2;
+            String [] potentialCards = cardDeck.get(realIndex);
+            String cardPng = potentialCards[rand.nextInt(potentialCards.length)];
+            return makeLabel(cardPng);
         }
 
         public JLabel makeLabel(String pngFile){      
